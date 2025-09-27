fe92ea5c1b5da2522d43f160abcf144df2fbe688
GrozniyMax <AzmEcmTzar@mail.ru>
2025-09-27 23:00:47 +0300

    lab2: fix backend

--- backend/src/main/java/com/maxim/lab1/controller/FlatController.java
+++ backend/src/main/java/com/maxim/lab1/controller/FlatController.java
@@ -10,17 +10,19 @@ import jakarta.validation.ValidationException;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+
+@RestController("/flat")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class FlatController {
@@ -29,44 +31,30 @@ public class FlatController {
     FlatService flatService;
     RemovalService removalService;
 
-    @PostMapping("/flat/create")
-    public String createFlat(@Valid @ModelAttribute("flatDto") FlatDto flatDto,
-                             BindingResult result,
+    @GetMapping("/page")
+    public Page<FlatDto> getFlats(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.ASC)
+                                  Pageable pageable,
+                                  @RequestParam(value = "name", required = false) String name) {
+        return flatService.getPage(pageable, name).map(mapper::toFlatDto);
+    }
+
+    @PostMapping("/create")
+    public void createFlat(@Valid FlatDto flatDto,
                              @RequestParam(value = "link", required = false, defaultValue = "false") Boolean link) {
-        if (result.hasErrors()) {
-            return "create";
-        }
+
         flatService.createFlat(mapper.toFlat(flatDto), link);
-        return "redirect:/page";
     }
 
-    @PostMapping("/flat/update")
-    public String updateFlat(@Valid @Validated(ValidationGroups.Update.class) @ModelAttribute("flatDto") FlatDto flatDto,
-                             BindingResult result,
-                             @RequestParam(value = "link", required = false, defaultValue = "false") Boolean link,
-                             Model model) {
-        try {
-            if (result.hasErrors()) {
-                return "update";
-            }
-            flatService.updateFlat(mapper.toFlat(flatDto), link);
-            return "redirect:/page";
-        } catch (ValidationException e) {
-            result.addError(new FieldError("flatDto", "id", e.getMessage()));
-            model.addAttribute("flatDto", flatDto);
-            return "update";
-        }
+    @PostMapping("/update")
+    public void updateFlat(@Valid @Validated(ValidationGroups.Update.class) FlatDto flatDto,
+                             @RequestParam(value = "link", required = false, defaultValue = "false") Boolean link) {
+
+        flatService.updateFlat(mapper.toFlat(flatDto), link);
     }
 
-    @GetMapping("/entity/delete")
-    public String deleteEntity(@RequestParam("entityType") String entityType, @RequestParam("id") Long id, Model model) {
-        try {
-            removalService.deleteById(entityType.toLowerCase(), id);
-            return "redirect:/page";
-        } catch (ValidationException e) {
-            model.addAttribute("validationError", e.getMessage());
-            return "delete";
-        }
+    @DeleteMapping("/{entity}/{id}")
+    public void deleteEntity(@PathVariable("entity") String entityType, @PathVariable("id") Long id) {
+        removalService.deleteById(entityType.toLowerCase(), id);
     }
 
 }
--- backend/src/main/java/com/maxim/lab1/controller/SpecialOperationsController.java
+++ backend/src/main/java/com/maxim/lab1/controller/SpecialOperationsController.java
@@ -6,22 +6,19 @@ import com.maxim.lab1.controller.dto.HouseDto;
 import com.maxim.lab1.model.Flat;
 import com.maxim.lab1.model.Transport;
 import com.maxim.lab1.service.SpecialOperationsService;
+import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
 
-@Controller
+@RestController("/operations")
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class SpecialOperationsController {
@@ -31,41 +28,20 @@ public class SpecialOperationsController {
     DtoMapper mapper;
 
     @PostMapping("/findCountByHouseGreaterThan")
-    public String findCountByHouseGreaterThan(@ModelAttribute HouseDto house, Model model) {
-        var result = specialOperationsService.findCountByHouseGreaterThan(mapper.toHouse(house));
-        model.addAttribute("count", result);
-        return "specials";
-    }
-
-    @GetMapping("/findAllByNameStartingWith")
-    public String findAllByNameStartingWith(@RequestParam("name") String name, Model model) {
-        var result = specialOperationsService.findAllByNameStartingWith(name).stream().map(mapper::toFlatDto).toList();
-        model.addAttribute("flats", result);
-        return "specials";
-    }
-
-    @GetMapping("/distinctTransport")
-    public String distinctTransport(Model model) {
-        var result = specialOperationsService.distinctTransport();
-        model.addAttribute("transports", result);
-        return "specials";
+    public long findCountByHouseGreaterThan(@Valid HouseDto house) {
+        return specialOperationsService.findCountByHouseGreaterThan(mapper.toHouse(house));
     }
 
     @GetMapping("/findMostExpensive")
-    public String findMostExpensive(
+    public FlatDto findMostExpensive(
             @RequestParam("id1") Long id1,
             @RequestParam("id2") Long id2,
-            @RequestParam("id3") Long id3,
-            Model model) {
-        var result = specialOperationsService.findMostExpensive(id1, id2, id3).map(mapper::toFlatDto);
-        model.addAttribute("flat", result.orElse(null));
-        return "specials";
+            @RequestParam("id3") Long id3) {
+        return specialOperationsService.findMostExpensive(id1, id2, id3).map(mapper::toFlatDto).orElse(null);
     }
 
     @GetMapping("/findTotalCost")
-    public String findTotalCost(Model model) {
-        var result = specialOperationsService.findTotalCost();
-        model.addAttribute("totalCost", result);
-        return "specials";
+    public long findTotalCost() {
+        return specialOperationsService.findTotalCost();
     }
 }
