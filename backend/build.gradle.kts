05561e427b9e519f753a1e063b5707423085e9e4
GrozniyMax <AzmEcmTzar@mail.ru>
2025-09-28 00:21:37 +0300

    lab2: fix generator

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,14 +1,17 @@
+import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
+
 plugins {
     java
     id("org.springframework.boot") version "3.5.5"
     id("io.spring.dependency-management") version "1.1.7"
-    id("io.freefair.lombok") version "8.14.2" // Use the latest version
+    id("io.freefair.lombok") version "8.14.2"
+    id("org.openapi.generator") version "7.4.0"
 
 }
 
 group = "com.maxim"
 version = "0.0.1-SNAPSHOT"
-description = "lab1"
+description = "lab2"
 
 java {
     toolchain {
@@ -46,6 +49,44 @@ tasks.withType<Jar> {
         attributes["Main-Class"] = "com.maxim.lab1.Lab1Application"
     }
 }
+
+tasks.register<GenerateTask>("generateCleanModels") {
+    generatorName.set("java")
+    inputSpec.set("$rootDir/../contract.yaml")
+    outputDir.set("$buildDir/temp-generated") // Генерируем во временную папку
+    modelPackage.set("com.example.model")
+
+    globalProperties.set(mapOf("models" to ""))
+    configOptions.set(
+        mapOf(
+            "useBeanValidation" to "true",
+            "openApiNullable" to "false"
+        )
+    )
+
+    doLast {
+        // Копируем только модели в финальную папку
+        copy {
+            from("$buildDir/temp-generated/src/main/java/com/example/model")
+            into("$buildDir/generated/src/main/java/com/example/model")
+        }
+        // Удаляем временную папку со всем мусором
+        delete("$buildDir/temp-generated")
+    }
+}
+
+sourceSets {
+    main {
+        java {
+            srcDir("$buildDir/generated/src/main/java")
+        }
+    }
+}
+
+tasks.compileJava {
+    dependsOn(tasks.named("generateCleanModels"))
+}
+
 
 tasks.withType<Test> {
     useJUnitPlatform()
